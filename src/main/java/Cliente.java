4be6800b1e52499d281885fe70c5783634f7d944
JimF04 <133442205+JimF04@users.noreply.github.com>
2023-10-25 20:27:24 -0600

    Correción de cámara

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,9 +1,24 @@
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.bytedeco.opencv.opencv_core.IplImage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.Date;
 
 
@@ -903,10 +918,13 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
     }
 
     private void cam_algeActionPerformed(java.awt.event.ActionEvent evt) {
-        CloudVision screen = new CloudVision();
-        screen.setVisible(true);
-
-
+        CameraFrame cameraFrame = new CameraFrame(new TextCallback() {
+            @Override
+            public void appendText(String text) {
+                pantalla_alge.append(text);
+            }
+        });
+        cameraFrame.setVisible(true);
     }
 
     private void AC_algeActionPerformed(java.awt.event.ActionEvent evt) {
@@ -920,9 +938,17 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
     }
 
     private void cam_logActionPerformed(java.awt.event.ActionEvent evt) {
-        CloudVision screen = new CloudVision();
-        screen.setVisible(true);
+        CameraFrame cameraFrame = new CameraFrame(new TextCallback() {
+            @Override
+            public void appendText(String text) {
+                pantalla_log.append(text);
+            }
+        });
+        cameraFrame.setVisible(true);
 
+    }
+    public interface TextCallback {
+        void appendText(String text);
     }
 
 
@@ -1082,5 +1108,153 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
 
         } catch (Exception e) {
             System.out.println(e);}
+    }
+}
+class CameraFrame extends JFrame {
+    private JButton btnTakePhoto;
+    private JLabel cameraLabel;
+    private OpenCVFrameGrabber grabber;
+    private IplImage currentFrame;
+    private final Lock grabberLock = new ReentrantLock();
+    private volatile boolean isRunning = true;
+    private Thread cameraThread;
+    private JButton btnCloseCamera;
+    private Cliente.TextCallback textCallback;
+
+
+    public CameraFrame(Cliente.TextCallback textCallback) {
+        this.textCallback = textCallback;
+
+        setTitle("Camera");
+        setSize(900, 600);
+        setLayout(new BorderLayout());
+
+        cameraLabel = new JLabel();
+        add(cameraLabel, BorderLayout.CENTER);
+
+        btnTakePhoto = new JButton("Take Photo");
+        btnTakePhoto.addActionListener(e -> captureAndProcess());
+
+        // Inicialización del botón "Close Camera"
+        btnCloseCamera = new JButton("Close Camera");
+        btnCloseCamera.addActionListener(e -> {
+            closeAndDispose(); // Método que cierra y libera la cámara y termina la ventana
+        });
+
+        JPanel southPanel = new JPanel(); // Panel para contener ambos botones
+        southPanel.add(btnTakePhoto);
+        southPanel.add(btnCloseCamera);
+        add(southPanel, BorderLayout.SOUTH);
+        startCamera();
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                isRunning = false;
+                if (cameraThread != null) {
+                    try {
+                        cameraThread.join(); // Asegúrate de que el thread de la cámara termine antes de proceder.
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                closeCamera(); // Crea este método
+            }
+        });
+
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+
+    public void startCamera() {
+        grabberLock.lock();
+        try {
+            if (grabber == null) {
+                grabber = new OpenCVFrameGrabber(0);
+                grabber.setImageWidth(832);
+                grabber.setImageHeight(468);
+                grabber.start();
+            }
+
+            Java2DFrameConverter converter = new Java2DFrameConverter();
+            cameraThread = new Thread(() -> {
+                try {
+                    while (isRunning) {
+                        org.bytedeco.javacv.Frame frame = grabber.grab();
+                        if (frame == null) break;
+                        currentFrame = new OpenCVFrameConverter.ToIplImage().convert(frame);
+                        SwingUtilities.invokeLater(() -> {
+                            ImageIcon icon = new ImageIcon(converter.getBufferedImage(frame));
+                            cameraLabel.setIcon(icon);
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            cameraThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            grabberLock.unlock();
+        }
+    }
+
+    public void captureAndProcess() {
+        if (currentFrame != null) {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+            String formattedNow = now.format(formatter);
+
+            File photosDir = new File("photos");
+            if (!photosDir.exists()) {
+                photosDir.mkdir();
+            }
+
+            String savedImagePath = "photos/" + formattedNow + ".png";
+            opencv_imgcodecs.cvSaveImage(savedImagePath, currentFrame);
+
+            try {
+                String extractedText = OCR.detectDocument(savedImagePath);
+                textCallback.appendText(extractedText); // Llama al método de la interfaz para agregar el texto
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            File file = new File(savedImagePath);
+            file.delete();
+            closeAndDispose();
+        } else {
+            System.out.println("No image to process.");
+        }
+    }
+
+    public void closeCamera() {
+        grabberLock.lock();
+        try {
+            if (grabber != null) {
+                grabber.stop();
+                grabber.release();
+                grabber = null;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            grabberLock.unlock();
+        }
+    }
+
+    public void closeAndDispose() {
+        isRunning = false;
+        if (cameraThread != null) {
+            try {
+                cameraThread.join(); // Asegúrate de que el thread de la cámara termine antes de proceder.
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        closeCamera();
+        dispose(); // Cierra la ventana
     }
 }
